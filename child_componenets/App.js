684c15925e2860b6f103083c6591d04d66a6ab68
James Junaidi <james.junaidi@gmail.com>
2020-06-15 11:51:14 -0700

    added child componenets

---
+++ child_componenets/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Footer from "./Footer"
+
+function App() {
+    return (
+        <div>
+        	<Header />
+        	<MainContent />
+            <Footer />
+        </div>
+    )
+}
+export default App
