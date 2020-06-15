848d2d245dd2309ec9ac0d25a387007292d72fc7
James Junaidi <james.junaidi@gmail.com>
2020-06-15 11:36:09 -0700

    MyInfo example

---
+++ MyInfoExample/MyInfo.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function MyInfo() {
+  return (
+    <div>
+      <h1> James </h1>
+      <p> My name is James </p>
+      <ul>
+        <li> New York </li>
+        <li> Tokyo </li>
+        <li> Seoul </li>
+      </ul>
+    </div>
+  )
+}
+
+
+export default MyInfo
---
+++ MyInfoExample/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import MyInfo from "./MyInfo"
+
+ReactDOM.render(<myInfo />, document.getElementById("root"))
